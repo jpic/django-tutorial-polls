1286c4332bc9c12d6d401e3cdf6eab06b179d2f7
jpic <jamespic@gmail.com>
2013-02-23 14:31:09 +0100

    tutorial01: Playing with the API: was_published_recently

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,9 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def __unicode__(self):
         return self.question
 
